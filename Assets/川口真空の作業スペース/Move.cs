0f628a8de6f36b304ec55a320f653dbd32364336
KawaguchiMasora <masora_22s1100736@nnn.ed.jp>
2023-10-28 16:53:20 +0900

    キュウビィ更新　箱生成追加(完成じゃない　馬鹿みたいに出てくるので上限、一回押して一個生成など)

--- Assets/川口真空の作業スペース/Move.cs
+++ Assets/川口真空の作業スペース/Move.cs
@@ -10,6 +10,14 @@ public class Move : MonoBehaviour
     private bool isGrounded = true;
     public Rigidbody2D rb;
 
+    private bool isControlPressed = false;
+    private bool isBPressed = false;
+
+    public GameObject boxPrefab;
+    public int maxBoxCount = 5; // 生成できる上限数
+
+    private int generatedBoxCount = 0; // 生成されたボックスの数を追跡
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -21,24 +29,39 @@ public class Move : MonoBehaviour
             // Rigidbody2Dがアタッチされていない場合、警告を表示
             Debug.LogWarning("Rigidbody2Dコンポーネントがアタッチされていません。");
         }
+        // InputManagerのSetActionにデリゲートを渡す
+        //まだ何も渡す設定していない
 
-    
     }
 
     void Update()
     {
-      //  float horizontalInput = Input.GetAxis("Horizontal");
-      //  float verticalInput = Input.GetAxis("Vertical");
+        //  float horizontalInput = Input.GetAxis("Horizontal");
+        //  float verticalInput = Input.GetAxis("Vertical");
 
-       // Vector2 movementForce = new Vector2(horizontalInput, verticalInput) * movementSpeed;
-       // rb.AddForce(movementForce);
+        // Vector2 movementForce = new Vector2(horizontalInput, verticalInput) * movementSpeed;
+        // rb.AddForce(movementForce);
 
-        float moveInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveInput * movementSpeed, rb.velocity.y);
+        //生成中は動けなくする
+        isControlPressed = Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
+        isBPressed = Input.GetKey(KeyCode.L);
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        //箱生成中じゃなければ動いてよし
+        if (!isControlPressed && !isBPressed)
         {
-            Jump(); 
+            float moveInput = Input.GetAxis("Horizontal");
+            rb.velocity = new Vector2(moveInput * movementSpeed, rb.velocity.y);
+        }
+
+        //箱生成中もしくは空中じゃなければジャンプしてよし
+        if (!isControlPressed && !isBPressed && Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+
+        if(isControlPressed && isBPressed)
+        {
+            BoxGeneration();
         }
     }
 
@@ -56,6 +79,39 @@ public class Move : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+        }
+    }
+
+    void BoxGeneration()
+    {
+        //箱生成処理
+        Debug.Log("箱生成中");
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log("↑");         
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("→");
+            // プレイヤーの位置を取得
+            Vector3 playerPosition = transform.position;
+
+            // x軸方向に10上にずらす
+            playerPosition.x += 10;
+
+            // 指定したPrefabを生成
+            Instantiate(boxPrefab, playerPosition, Quaternion.identity);
+
+            // 生成回数をカウントアップ
+            generatedBoxCount++;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("←");
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log("↓");
         }
     }
 }
