8329143f3f46aa378a5721c38a840a0d5cce9709
ESO-gardo-kia <114640824+ESO-gardo-kia@users.noreply.github.com>
2023-11-04 15:38:04 +0900

    ハコボーイジャンプアニメーション追加

--- Assets/江曽拓摩の作業スペース/anime_test.cs
+++ Assets/江曽拓摩の作業スペース/anime_test.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class anime_test : MonoBehaviour
 {
     public Animator animator;
+
+    [SerializeField] public Rigidbody2D rb;
+
+    public float jumpforce = 6f;
+    public float jump_num = 100;
+    public float jump_second = 0.1f;
+
+    public bool isJumping = false;//ジャンプ出来るか否か
+    public bool isJumping_running = false;//ジャンプ処理中か否か
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -30,5 +39,54 @@ public class anime_test : MonoBehaviour
     {
         if (Input.GetKeyUp(KeyCode.A)) animator.SetFloat("walk", 0);
         if (Input.GetKeyUp(KeyCode.D)) animator.SetFloat("walk", 0);
+        if (Input.GetKeyDown(KeyCode.Space) //buttonが押されていて
+            && !isJumping_running)//ジャンプ処理が行われていない場合
+        {
+            if (isJumping && !isJumping_running)
+            {
+                Debug.Log("ジャンプ");
+                StartCoroutine("JunpMove");
+                isJumping = false;
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping_running = false;
+        }
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ground")) isJumping = true;
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping_running = false;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ground")) isJumping = false;
+    }
+    IEnumerator JunpMove()
+    {
+        rb.velocity = Vector3.zero;
+        isJumping_running = true;
+        float ju_fo = jumpforce;
+        for (int i = 0; i <= jump_num && isJumping_running == true; i++)
+        {
+            if (Input.GetKey(KeyCode.Space) && i == 0)
+            {
+                rb.AddForce(Vector2.up * (jumpforce), ForceMode2D.Impulse);
+                yield return new WaitForSeconds(jump_second);
+            }
+            if (Input.GetKey(KeyCode.Space) && i != 0)
+            {
+                ju_fo = ju_fo / 2f;
+                rb.AddForce(Vector2.up * (ju_fo), ForceMode2D.Impulse);
+                yield return new WaitForSeconds(jump_second);
+            }
+        }
     }
 }
