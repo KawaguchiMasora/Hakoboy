b3e5cf54827eb5e130d830062a936be6097d4bd8
ESO-gardo-kia <114640824+ESO-gardo-kia@users.noreply.github.com>
2023-12-09 13:06:10 +0900

    立ちモーション

--- Assets/江曽拓摩の作業スペース/テクスチャー/アニメーション/死亡アニメーション/Death_animation.cs
+++ Assets/江曽拓摩の作業スペース/テクスチャー/アニメーション/死亡アニメーション/Death_animation.cs
@@ -6,16 +6,6 @@ public class Death_animation : MonoBehaviour
 {
     [SerializeField]
     private GameObject ps_obj;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void Death_Anime()
     {
         Instantiate(ps_obj, this.transform.position, Quaternion.identity);
