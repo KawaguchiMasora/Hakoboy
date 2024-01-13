7542e9ec0a65622a8ddec647095bbbbd141884ce
KawaguchiMasora <masora_22s1100736@nnn.ed.jp>
2024-01-13 16:08:16 +0900

    ゴール判定作成

--- Assets/川口真空の作業スペース/Goal.cs
+++ Assets/川口真空の作業スペース/Goal.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private BoxCollider2D boxCollider;
+    public GameObject targetObject;
+    public string targetTag = "Player";
+    [SerializeField] Rigidbody2D rb;
+
+    bool Goalarea = false;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Goalarea = true && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("ÉSÅ[ÉãÇµÇ‹ÇµÇΩ");
+            Move otherScript = targetObject.GetComponent<Move>();
+            otherScript.enabled = false;
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Goalarea = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(targetTag))
+        {
+            Goalarea = false;
+        }
     }
 }
