fdcf7283ae015bef7debc69f52afc4c667071541
kentaulos <sotan.2006@icloud.com>
2024-01-27 15:32:47 +0900

    ゴールのシーン移行

--- Assets/関根正秀の作業スペース/スクリプト/DoorManager.cs
+++ Assets/関根正秀の作業スペース/スクリプト/DoorManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DoorManager : MonoBehaviour
 {
     private Animator anim;
+    public string scenename;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +19,13 @@ public class DoorManager : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             anim.SetTrigger("open");
+            Invoke("nextscene", 2);
         }
+    }
+
+    private void nextscene()
+    {
+        SceneManager.LoadScene(scenename);
     }
     // Update is called once per frame
     void Update()
